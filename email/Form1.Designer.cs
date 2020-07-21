45d96f4e553d037d640d7409c823b5841e748ffc
ckp <2834880181@qq.com>
2020-07-21 09:34:08 +0800

    添加项目文件。

---
+++ email/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace email
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_from = new System.Windows.Forms.TextBox();
+            this.txt_pwd = new System.Windows.Forms.TextBox();
+            this.txt_to = new System.Windows.Forms.TextBox();
+            this.txt_subject = new System.Windows.Forms.TextBox();
+            this.txt_body = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.txt_statue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_from
+            // 
+            this.txt_from.Location = new System.Drawing.Point(90, 64);
+            this.txt_from.Multiline = true;
+            this.txt_from.Name = "txt_from";
+            this.txt_from.Size = new System.Drawing.Size(177, 31);
+            this.txt_from.TabIndex = 0;
+            // 
+            // txt_pwd
+            // 
+            this.txt_pwd.Location = new System.Drawing.Point(452, 64);
+            this.txt_pwd.Multiline = true;
+            this.txt_pwd.Name = "txt_pwd";
+            this.txt_pwd.Size = new System.Drawing.Size(177, 31);
+            this.txt_pwd.TabIndex = 1;
+            // 
+            // txt_to
+            // 
+            this.txt_to.Location = new System.Drawing.Point(90, 152);
+            this.txt_to.Multiline = true;
+            this.txt_to.Name = "txt_to";
+            this.txt_to.Size = new System.Drawing.Size(177, 31);
+            this.txt_to.TabIndex = 2;
+            // 
+            // txt_subject
+            // 
+            this.txt_subject.Location = new System.Drawing.Point(90, 228);
+            this.txt_subject.Multiline = true;
+            this.txt_subject.Name = "txt_subject";
+            this.txt_subject.Size = new System.Drawing.Size(177, 31);
+            this.txt_subject.TabIndex = 3;
+            // 
+            // txt_body
+            // 
+            this.txt_body.Location = new System.Drawing.Point(90, 307);
+            this.txt_body.Multiline = true;
+            this.txt_body.Name = "txt_body";
+            this.txt_body.Size = new System.Drawing.Size(270, 112);
+            this.txt_body.TabIndex = 4;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(452, 298);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 5;
+            this.btn_send.Text = "发送";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // txt_statue
+            // 
+            this.txt_statue.Location = new System.Drawing.Point(607, 326);
+            this.txt_statue.Multiline = true;
+            this.txt_statue.Name = "txt_statue";
+            this.txt_statue.Size = new System.Drawing.Size(97, 32);
+            this.txt_statue.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(331, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 27);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "邮 箱";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(384, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "密 码";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "接收者";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "标 题";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 346);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "正文";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(644, 298);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "状态";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_statue);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.txt_body);
+            this.Controls.Add(this.txt_subject);
+            this.Controls.Add(this.txt_to);
+            this.Controls.Add(this.txt_pwd);
+            this.Controls.Add(this.txt_from);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_from;
+        private System.Windows.Forms.TextBox txt_pwd;
+        private System.Windows.Forms.TextBox txt_to;
+        private System.Windows.Forms.TextBox txt_subject;
+        private System.Windows.Forms.TextBox txt_body;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox txt_statue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ email/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace email
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                send_mail();
+                txt_statue.Text = "success!";
+                
+            }
+            catch
+            {
+                txt_statue.Text = "error!";
+            }
+        }
+        /// <summary>
+        /// 发送邮件方法
+        /// </summary>
+        void send_mail()
+        {
+            MailMessage mms = new MailMessage();
+            mms.From = new MailAddress(txt_from.Text);
+            mms.To.Add(txt_to.Text);
+            mms.Subject = txt_subject.Text;
+            mms.Body = txt_body.Text;
+            SmtpClient sct = new SmtpClient();
+            sct.Credentials = new System.Net.NetworkCredential(txt_from.Text,txt_pwd.Text);
+            sct.Host = "smtp.163.com";
+            sct.Send(mms);
+        }
+    }
+}
